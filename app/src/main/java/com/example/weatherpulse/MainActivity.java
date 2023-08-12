aa30ff687b55d565f3a490dcae4e54310d22cdb8
akshmangal005 <akashmangal79@gmail.com>
2023-08-12 12:58:56 +0530

    new feature

--- app/src/main/java/com/example/weatherpulse/MainActivity.java
+++ app/src/main/java/com/example/weatherpulse/MainActivity.java
@@ -1,33 +1,52 @@
 package com.example.weatherpulse;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 import org.json.JSONException;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,6 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
     public static String cname = null , msg = null , hmdty = null , temp_in_c = null , temp_in_f = null , wind_speed_in_kh = null , wind_speed_in_mh = null;
     public static Bitmap img = null;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private final static int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,11 +79,72 @@ public class MainActivity extends AppCompatActivity {
         enter_city = findViewById(R.id.search_city);
         img_icon = findViewById(R.id.weather_image);
         weather_message = findViewById(R.id.weather_message);
+        ImageButton curr_loc_btn = findViewById(R.id.curr_loc);
+
+        final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+        FusedLocationProviderClient fusedLocationClient;
+        LocationCallback locationCallback;
+
+
 
         SharedPreferences getSharedValues = getSharedPreferences("weatherdetails",MODE_PRIVATE);
         String temp = getSharedValues.getString("cname","CITY");
         getInfoFromAPI(temp);
 
+
+        private void checkLocationPermission() {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted, request it
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            } else {
+                // Permission is already granted, proceed with location access
+                startLocationUpdates();
+            }
+        }
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted, proceed with location access
+                    startLocationUpdates();
+                } else {
+                    // Permission denied, handle accordingly
+                }
+            }
+        }
+
+        private void startLocationUpdates() {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(1000); // Update location every second
+
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult != null) {
+                        Location location = locationResult.getLastLocation();
+                        if (location != null) {
+                            // Handle location updates here
+                            double latitude = location.getLatitude();
+                            double longitude = location.getLongitude();
+
+                            // Do something with the latitude and longitude values
+                        }
+                    }
+                }
+            };
+
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        }
+
+        private void stopLocationUpdates() {
+            fusedLocationClient.removeLocationUpdates(locationCallback);
+        }
 
         enter_btn.setOnClickListener(view -> {
 
@@ -81,6 +164,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     private boolean datadisabled(Context context){
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
